759408e8ff8fb6feb8d12085434561867147c09b
lkamil <lkamil@protonmail.com>
2018-09-28 16:22:03 +0200

    add comments + refactor

--- spiro.hs
+++ spiro.hs
@@ -10,68 +10,74 @@ main
     spiro   -- picture to display
 
 -- parameters
-pointSize = 1
-density = 2000
-innerRadius = 1
-outerRadius = 112
-distance = 100
-loops = 2
+density = 2000      -- higher density results in smoother lines
+innerRadius = 1     -- radius of the inner circle
+outerRadius = 112   -- radius of the outer circle
+distance = 100      -- distance from the centre of the inner circle
+loops = 2           -- count of 360° loops
 
+-- spiro combines different trochoids
 spiro :: Picture
 spiro = pictures
-    [ color darkRed $ line (drawEpitrochoid (0, 0) 0)
-    , color rosee $ line (drawHypocycloid (0, 0) 0)
-    , color lightGreen $ line (drawEpicycloid (0, 0) 0)
-    , color darkblue $ line (drawHypotrochoid (0, 0) 0)
+    [ color darkred $ line drawHypotrochoid
+    , color rosee $ line drawHypocycloid
+    , color lightgreen $ line drawEpicycloid
+    , color darkblue $ line drawEpitrochoid
     ]
-
-toRad :: Float -> Float
-toRad angle = angle * pi / 180
 
 -- colors
 cerise = light $ makeColorI 255 7 79 255
 purple = light $ light $ makeColorI 0 0 153 255
 citrone = makeColorI 255 232 0 255
-lightGreen = makeColorI 0 153 153 255
+lightgreen = makeColorI 0 153 153 255
 skin = makeColorI 255 204 153 255
 darkblue = makeColorI 0 80 221 255
 rosee = makeColorI 255 102 153 255
-babyBlue = makeColorI 115 254 255 255
-darkRed = makeColorI 153 0 36 255
-darkPurple = makeColorI 38 0 77 255
+babyblue = makeColorI 115 254 255 255
+darkred = makeColorI 153 0 36 255
+darkpurple = makeColorI 38 0 77 255
 
-drawHypotrochoid :: Point -> Float -> Path
-drawHypotrochoid (lpx, lpy) angle
+drawHypotrochoid = drawHypotrochoidH (0, 0) 0
+drawHypocycloid = drawHypocycloidH (0, 0) 0
+drawEpitrochoid = drawEpitrochoidH (0, 0) 0
+drawEpicycloid = drawEpicycloidH (0, 0) 0
+
+drawHypotrochoidH :: Point -> Float -> Path
+drawHypotrochoidH (lpx, lpy) angle
     | angle >= 360 * loops = []
-    | otherwise = (newX, newY) : drawHypotrochoid (newX, newY) (angle + step)
+    | otherwise = (newX, newY) : drawHypotrochoidH (newX, newY) (angle + step)
     where
         newX = (outerRadius - innerRadius) * (cos $ toRad angle) + distance * (cos (((outerRadius - innerRadius) / innerRadius) * (toRad angle)))
         newY = (outerRadius - innerRadius) * (sin $ toRad angle) - distance * (sin (((outerRadius - innerRadius) / innerRadius) * (toRad angle)))
         step = 360 / density
 
-drawEpicycloid :: Point -> Float -> Path
-drawEpicycloid (lpx, lpy) angle
+drawEpicycloidH :: Point -> Float -> Path
+drawEpicycloidH (lpx, lpy) angle
     | angle >=  360 * loops= []
-    | otherwise = (newX, newY) : drawEpicycloid (newX, newY) (angle + step)
+    | otherwise = (newX, newY) : drawEpicycloidH (newX, newY) (angle + step)
     where
         newX = (outerRadius + innerRadius) * (cos $ toRad angle) - innerRadius * (cos (((outerRadius + innerRadius) / innerRadius) * (toRad angle)))
         newY = (outerRadius + innerRadius) * (sin $ toRad angle) - innerRadius * (sin (((outerRadius + innerRadius) / innerRadius) * (toRad angle)))
         step = 360 / density
 
-drawHypocycloid :: Point -> Float -> Path
-drawHypocycloid (lpx, lpy) angle
+drawHypocycloidH :: Point -> Float -> Path
+drawHypocycloidH (lpx, lpy) angle
     | angle >= 360 * loops = []
-    | otherwise = (newX, newY) : drawHypocycloid (newX, newY) (angle + step)
+    | otherwise = (newX, newY) : drawHypocycloidH (newX, newY) (angle + step)
     where
         newX = (outerRadius - innerRadius) * (cos $ toRad angle) + innerRadius * (cos (((outerRadius - innerRadius) / innerRadius) * (toRad angle)))
         newY = (outerRadius - innerRadius) * (sin $ toRad angle) - innerRadius * (sin (((outerRadius - innerRadius) / innerRadius) * (toRad angle)))
         step = 360 / density
 
-drawEpitrochoid :: Point -> Float -> Path
-drawEpitrochoid (lpx, lpy) angle
+drawEpitrochoidH :: Point -> Float -> Path
+drawEpitrochoidH (lpx, lpy) angle
     | angle >= 360 * loops = []
-    | otherwise = (newX, newY) : drawEpitrochoid (newX, newY) (angle + step)
+    | otherwise = (newX, newY) : drawEpitrochoidH (newX, newY) (angle + step)
     where
         newX = (outerRadius + innerRadius) * (cos $ toRad angle) + distance * (cos (((outerRadius + innerRadius) / innerRadius) * (toRad angle)))
         newY = (outerRadius + innerRadius) * (sin $ toRad angle) - distance * (sin (((outerRadius + innerRadius) / innerRadius) * (toRad angle)))
         step = 360 / density
+
+toRad :: Float -> Float
+toRad angle = angle * pi / 180
+
